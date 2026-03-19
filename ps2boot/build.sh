ba69b1b06c1239236c272af9bd7dc297df5c1e73
ReyFxck <ReyFxck@users.noreply.github.com>
2026-03-19 07:13:47 +0000

    Restore embedded SMW ROM and strip release ELF

--- ps2boot/build.sh
+++ ps2boot/build.sh
@@ -5,5 +5,6 @@ cd "$(dirname "$0")"
 
 rm -f main.o ps2_video.o smw_blob.o ps2snes2005_boot.elf
 make
+mips64r5900el-ps2-elf-strip --strip-unneeded ps2snes2005_boot.elf
 cp -f ps2snes2005_boot.elf /sdcard/ps2/
 ls -lh ps2snes2005_boot.elf /sdcard/ps2/ps2snes2005_boot.elf
