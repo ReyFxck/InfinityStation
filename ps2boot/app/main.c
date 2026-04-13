69b102e4099121c567d3b0dda3aa86eb1681d952
ReyFxck <thomas462495@gmail.com>
2026-04-13 22:34:19 +0000

    Remove runtime profiling overlay from PS2 frontend

--- ps2boot/app/main.c
+++ ps2boot/app/main.c
@@ -26,70 +26,12 @@ __attribute__((weak)) int select_menu_actions_game_vsync_enabled(void);
 static uint32_t g_prev_buttons = 0;
 
 
-extern const char *app_core_prof_get_line1(void);
-extern const char *app_core_prof_get_line2(void);
-extern const char *app_core_prof_get_line3(void);
-extern const char *app_core_prof_get_line4(void);
-static unsigned g_core_prof_frames = 0;
-static unsigned long long g_core_prof_run_cycles = 0;
-static unsigned long long g_core_prof_post_cycles = 0;
-static unsigned long long g_core_prof_total_cycles = 0;
-
-static inline unsigned app_prof_read_count(void)
-{
-    unsigned value;
-    __asm__ __volatile__("mfc0 %0, $9" : "=r"(value));
-    return value;
-}
-
-static inline float app_prof_cycles_to_ms(unsigned long long cycles, unsigned frames)
-{
-    if (!frames)
-        return 0.0f;
-
-    return (float)((double)cycles / (double)frames / 294912.0);
-}
-
 static void app_prof_commit(unsigned run_cycles, unsigned post_cycles, unsigned total_cycles)
 {
-    char l1[48];
-    char l2[48];
-
-    g_core_prof_run_cycles += run_cycles;
-    g_core_prof_post_cycles += post_cycles;
-    g_core_prof_total_cycles += total_cycles;
-    g_core_prof_frames++;
-
-    if (g_core_prof_frames >= 30u) {
-        snprintf(l1, sizeof(l1), "RUN %.2f | POST %.2f",
-                 app_prof_cycles_to_ms(g_core_prof_run_cycles, g_core_prof_frames),
-                 app_prof_cycles_to_ms(g_core_prof_post_cycles, g_core_prof_frames));
-
-        snprintf(l2, sizeof(l2), "FRAME %.2f",
-                 app_prof_cycles_to_ms(g_core_prof_total_cycles, g_core_prof_frames));
-
-        {
-            const char *core_l1 = app_core_prof_get_line1();
-            const char *core_l2 = app_core_prof_get_line2();
-            const char *core_l3 = app_core_prof_get_line3();
-            const char *core_l4 = app_core_prof_get_line4();
-
-            if (core_l1 && core_l1[0])
-                ps2_video_set_debug(
-                    core_l1,
-                    (core_l2 && core_l2[0]) ? core_l2 : l2,
-                    (core_l3 && core_l3[0]) ? core_l3 : "",
-                    (core_l4 && core_l4[0]) ? core_l4 : ""
-                );
-            else
-                ps2_video_set_debug(l1, l2, NULL, NULL);
-        }
-
-        g_core_prof_frames = 0;
-        g_core_prof_run_cycles = 0;
-        g_core_prof_post_cycles = 0;
-        g_core_prof_total_cycles = 0;
-    }
+    (void)run_cycles;
+    (void)post_cycles;
+    (void)total_cycles;
+    ps2_video_set_debug("", "", "", "");
 }
 
 static void die(const char *msg)
@@ -135,7 +77,6 @@ int main(int argc, char *argv[])
         uint32_t pressed;
         int menu_consumed;
         app_request_t req;
-        unsigned t0, t1, t2;
 
         ps2_input_poll();
         buttons = ps2_input_buttons();
@@ -160,12 +101,9 @@ int main(int argc, char *argv[])
 
         if (menu_consumed)
             continue;
-        t0 = app_prof_read_count();
         retro_run();
-        t1 = app_prof_read_count();
         app_runtime_finish_frame();
-        t2 = app_prof_read_count();
-        app_prof_commit(t1 - t0, t2 - t1, t2 - t0);
+        app_prof_commit(0, 0, 0);
         g_prev_buttons = buttons;
     }
 
