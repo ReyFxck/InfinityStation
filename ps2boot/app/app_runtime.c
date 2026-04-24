35228fb9d6929760deece8962e183090eb2549aa
root <root@localhost.localdomain>
2026-04-24 23:17:21 +0000

    frontend: abrir menu segurando SELECT

--- ps2boot/app/app_runtime.c
+++ ps2boot/app/app_runtime.c
@@ -6,8 +6,60 @@
 #include "ps2_video.h"
 
 #include <libpad.h>
+#include <time.h>
 
 #include "ps2_menu.h"
+
+#define APP_MENU_SELECT_HOLD_SECONDS 2
+#define APP_MENU_SELECT_HOLD_TICKS ((clock_t)(APP_MENU_SELECT_HOLD_SECONDS * CLOCKS_PER_SEC))
+#define APP_MENU_SELECT_HOLD_FALLBACK_FRAMES 120u
+
+static clock_t g_select_hold_start = 0;
+static unsigned g_select_hold_frames = 0;
+static int g_select_hold_triggered = 0;
+
+static void app_runtime_reset_select_hold(void)
+{
+    g_select_hold_start = 0;
+    g_select_hold_frames = 0;
+    g_select_hold_triggered = 0;
+}
+
+static int app_runtime_select_held_long_enough(uint32_t buttons)
+{
+    clock_t now;
+
+    if (!(buttons & PAD_SELECT)) {
+        app_runtime_reset_select_hold();
+        return 0;
+    }
+
+    if (g_select_hold_triggered)
+        return 0;
+
+    g_select_hold_frames++;
+
+    now = clock();
+
+    if (now != (clock_t)-1) {
+        if (g_select_hold_start == 0) {
+            g_select_hold_start = now ? now : 1;
+            return 0;
+        }
+
+        if ((clock_t)(now - g_select_hold_start) >= APP_MENU_SELECT_HOLD_TICKS) {
+            g_select_hold_triggered = 1;
+            return 1;
+        }
+    }
+
+    if (g_select_hold_frames >= APP_MENU_SELECT_HOLD_FALLBACK_FRAMES) {
+        g_select_hold_triggered = 1;
+        return 1;
+    }
+
+    return 0;
+}
 
 int app_runtime_handle_menu(uint32_t buttons, uint32_t pressed, uint32_t *prev_buttons)
 {
@@ -56,7 +108,7 @@ int app_runtime_handle_menu(uint32_t buttons, uint32_t pressed, uint32_t *prev_b
         return 1;
     }
 
-    if (pressed & PAD_SELECT) {
+    if (app_runtime_select_held_long_enough(buttons)) {
         ps2_audio_pause();
         ps2_menu_open();
         app_state_set_mode(APP_MODE_MENU);
